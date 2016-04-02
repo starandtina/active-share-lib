bee5a36bd0f822ac94957a3ce6d21d12021dedaf
starandtina <kandmforever@126.com>
2016-04-02 21:12:37 +0800

    chore(build): build

--- src/async-render.js
+++ src/async-render.js
@@ -1,4 +1,5 @@
-import 'babel-polyfill';
+import 'babel-polyfill'
+import fetch from 'isomorphic-fetch'
 
 function renderComponent() {
   return new Promise((resolve, reject) => {
@@ -10,6 +11,41 @@ function renderComponent() {
 
 export default async function render() {
   var file = await renderComponent();
-
+  main();
   console.log(file);
+}
+
+function getUser(cb) {
+  return new Promise((resolve, reject) => {
+    fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(quote => {
+        if (typeof cb === 'function') {
+          cb(quote)
+        }
+        resolve(quote)
+      })
+      .catch(reject);
+  });
+}
+
+function printMessage(msg) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log(msg);
+      resolve(msg);
+    }, 2000);
+  });
+}
+
+async function main() {
+  try {
+    console.log('Gonna get a quote');
+    const quote = await getUser();
+    console.log(quote);
+    await printMessage('test msg');
+    console.log('a message was printed after 2s');
+  } catch (error) {
+    console.error(error);
+  }
 }
